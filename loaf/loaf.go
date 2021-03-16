db3f78f18d0ae2d8f9c292b77d3cbc9608feb8e4
Liu <jliu1@illumina.com>
2021-03-16 08:42:58 -0700

    optimized batch size getter-setter

--- loaf/loaf.go
+++ loaf/loaf.go
@@ -99,6 +99,7 @@ func LoafOfVcf(ch chan []string, params *LoafParams) *Loaf {
 		// GT:SQ:AD:AF:F1R2:F2R1:DP:SB:MB	0/1:12.58:2,1:0.333:1,0:1,1:3:1,1,1,0:2,0,1,0
 		sp2 := strings.Split(formatValueField, ":")
 		if len(sp2) < 4 {
+
 			continue
 		}
 		af, err := strconv.ParseFloat(sp2[3], 64)
